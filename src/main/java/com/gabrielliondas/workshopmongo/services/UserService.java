a542746195fe9680ec4ac4aa932d588630e13b1e
Gabriel-Liondas <liondas.gabriel@gmail.com>
2023-09-27 10:17:44 -0300

    Get user by ID

--- src/main/java/com/gabrielliondas/workshopmongo/services/UserService.java
+++ src/main/java/com/gabrielliondas/workshopmongo/services/UserService.java
@@ -1,11 +1,13 @@
 package com.gabrielliondas.workshopmongo.services;
 
 import com.gabrielliondas.workshopmongo.domain.User;
+import com.gabrielliondas.workshopmongo.exception.ObjectNotFoundException;
 import com.gabrielliondas.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +17,11 @@ public class UserService {
 
     public List<User> findAll(){
         return repo.findAll();
+    }
+
+
+    public User findById(String id) {
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado"));
     }
 }
