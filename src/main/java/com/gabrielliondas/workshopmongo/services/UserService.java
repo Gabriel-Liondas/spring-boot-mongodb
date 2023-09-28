938b40fab91fada60fbfb64040c2ea393e541858
Gabriel-Liondas <liondas.gabriel@gmail.com>
2023-09-28 09:35:22 -0300

    Insert a User by Post method

--- src/main/java/com/gabrielliondas/workshopmongo/services/UserService.java
+++ src/main/java/com/gabrielliondas/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.gabrielliondas.workshopmongo.services;
 
 import com.gabrielliondas.workshopmongo.domain.User;
+import com.gabrielliondas.workshopmongo.dto.UserDTO;
 import com.gabrielliondas.workshopmongo.exception.ObjectNotFoundException;
 import com.gabrielliondas.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,5 +24,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado"));
+    }
+
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(),objDto.getName(), objDto.getEmail());
     }
 }
