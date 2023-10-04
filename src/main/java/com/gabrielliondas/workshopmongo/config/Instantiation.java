9da8402c426578bbd3387b4bc07d7f5d8e3bbf01
Gabriel-Liondas <liondas.gabriel@gmail.com>
2023-10-04 09:56:50 -0300

    Referencing all Posts from an User

--- src/main/java/com/gabrielliondas/workshopmongo/config/Instantiation.java
+++ src/main/java/com/gabrielliondas/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.TimeZone;
 
@@ -40,5 +41,8 @@ public class Instantiation implements CommandLineRunner {
         Post post2 = new Post(null,sdf.parse("23/03/2018"),"Bom dia","Acordei feliz hoje", new AuthorDTO(maria));
 
         postRepository.saveAll(Arrays.asList(post1,post2));
+
+        maria.setPosts(Arrays.asList(post1,post2));
+        userRepository.save(maria);
     }
 }
